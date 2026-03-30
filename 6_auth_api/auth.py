aa11be33de5c8ece037e026acd055761b007b8d5
roma <migalroman07@gmail.com>
2026-03-30 14:34:45 +0300

    edit auth.py

--- 6_auth_api/auth.py
+++ 6_auth_api/auth.py
@@ -2,21 +2,24 @@ import requests
 from requests_oauth2 import OAuth2BearerToken
 from requests.auth import HTTPDigestAuth, HTTPBasicAuth
 
+user = 'roma1'
+passwd = '123'
+
 # Basic auth.
-url = "https://httpbin.org/basic-auth/roma1/123"
+url = f"https://httpbin.org/basic-auth/{user}/{passwd}"
 basic = HTTPBasicAuth('roma1', '123')
 response = requests.get(url, auth=basic)
-print(response)
+print(response.status_code)
 
 # Bearer
 url = "https://httpbin.org/bearer"
-token = OAuth2BearerToken('Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InJvbWExIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.PJpvreozcDd4-QjiKIMSmRU4BLFwmVaQCDDjViW-BS4')
-response = requests.get(url, auth=token)
-print(response)
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InJvbWExIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.PJpvreozcDd4-QjiKIMSmRU4BLFwmVaQCDDjViW-BS4"
+headers = { "Authorization": f"Bearer {token}" }
+response = requests.get(url, headers=headers)
+print(response.status_code)
 
 # Digest-auth
-url = "https://httpbin.org/digest-auth/auth/roma1/123"
-basic = HTTPDigestAuth('roma1', '123')
-response = requests.get(url, auth=basic) 
-print(response)
+url = f"https://httpbin.org/digest-auth/auth/{user}/{passwd}"
+response = requests.get(url, auth=HTTPDigestAuth('roma1', '123')) 
+print(response.status_code)
 
